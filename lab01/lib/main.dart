2094cb653820126ea9fb58a54cec2510afd9e78f
tuanit168 <tuanit168@gmail.com>
2021-09-17 02:22:20 +1000

    Create a stateful widget named RandomWords

--- lab01/lib/main.dart
+++ lab01/lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,9 +14,23 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           // child: const Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+          // child: Text(wordPair.asPascalCase, style: TextStyle(fontSize: 50)),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();      // NEW
+    return Text(wordPair.asPascalCase);
   }
 }
