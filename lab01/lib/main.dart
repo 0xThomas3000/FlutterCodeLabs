f52ce1cddf4ccfebaa70b851da2d482127910e24
tuanit168 <tuanit168@gmail.com>
2021-09-17 19:21:51 +1000

    Changing color using Color Scheme

--- lab01/lib/main.dart
+++ lab01/lib/main.dart
@@ -1,75 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 class RandomWords extends StatefulWidget {
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
+
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];   // saving suggested word pairings
+  final _suggestions = <WordPair>[]; // saving suggested word pairings
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        // NEW lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called once per suggested 
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a 
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
-      itemBuilder: (BuildContext _context, int i) {
-        // Add a one-pixel-high divider widget before each row 
-        // in the ListView.
-        if (i.isOdd) {
-          return Divider();
-        }
+        padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return Divider();
+          }
 
-        // The syntax "i ~/ 2" divides i by 2 and returns an 
-        // integer result.
-        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings 
-        // in the ListView,minus the divider widgets.
-        final int index = i ~/ 2;
-        // If you've reached the end of the available word
-        // pairings...
-        if (index >= _suggestions.length) {
-          // ...then generate 10 more and add them to the 
-          // suggestions list.
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();     
+    // final wordPair = WordPair.random();
     // return Text(wordPair.asPascalCase);
-    return Scaffold (               
+    return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey,
+        ).copyWith(
+          secondary: Colors.green,
+        ),
+        textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.purple)),
+      ),
       home: RandomWords(),
     );
   }
 }
+
 void main() => runApp(MyApp());
